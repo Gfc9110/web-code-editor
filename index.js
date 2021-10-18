acaccba3026f57879ec42fc744b060831609b126
Giorgio Fausto Canu <gfc9110@gmail.com>
2021-10-19 01:16:17 +0200

    nice things

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ String.prototype.splice = function (index, count, add) {
 let cursor = { x: 0, y: 0 }
 
 function repositonCursor() {
+  cursor.x = Math.floor(cursor.x);
+  cursor.y = Math.floor(cursor.y);
   if (linesContainer.childNodes[cursor.y]) {
     cursor.x = Math.min(linesContainer.childNodes[cursor.y].textContent.length, cursor.x);
   }
@@ -15,14 +17,35 @@ function repositonCursor() {
   cursorElement.style.top = (cursor.y * 30) + "px"
 }
 
+function lineExists(i) {
+  return !!linesContainer.childNodes[i];
+}
 
-function moveCursorRight() {
-  cursor.x++;
+function focusLine(i) {
+  linesContainer.childNodes[i]?.focus();
+}
+
+function deleteLine(i) {
+  linesContainer.childNodes[i]?.remove();
+}
+
+function SetCursorX(val) {
+  cursor.x = val
   repositonCursor();
 }
 
-function moveCursorLeft() {
-  cursor.x--;
+function SetCursorY(val) {
+  cursor.y = Math.min(linesContainer.childNodes.length - 1, val)
+  repositonCursor();
+}
+
+function moveCursorRight(val = 1) {
+  cursor.x += val;
+  repositonCursor();
+}
+
+function moveCursorLeft(val = 1) {
+  cursor.x -= val;
   cursor.x = Math.max(0, cursor.x);
   repositonCursor();
 }
@@ -46,32 +69,47 @@ var linesObserver = new MutationObserver((mutations) => {
     marginLine.textContent = (marginsContainer.childNodes?.length || 0) + 1
     marginsContainer.appendChild(marginLine)
   }
+  while (mutations[0].target.childNodes.length < (marginsContainer.childNodes?.length || 0)) {
+    marginsContainer.lastChild.remove();
+  }
 })
 
 linesObserver.observe(linesContainer, { childList: true });
 
-function newLine() {
+function newLine(active = false) {
   const line = document.createElement("div");
   line.className = "line";
-  line.tabIndex = 0;
-  line.addEventListener("keydown", ({ key }) => {
-    if (interpretSpecial(key)) return;
-    line.textContent = line.textContent.splice(cursor.x, 0, key);
-    moveCursorRight();
-  })
+  if (active) {
+    line.tabIndex = 0;
+    line.addEventListener("keydown", (event) => {
+      let result = interpretSpecial(event);
+      if (result && typeof result == "string") {
+        line.textContent = line.textContent.splice(cursor.x, 0, result);
+        moveCursorRight(result.length);
+      } else if (!result) {
+        line.textContent = line.textContent.splice(cursor.x, 0, event.key);
+        moveCursorRight(event.key.length);
+      }
+    })
+    line.addEventListener("mousedown", (event) => {
+      //event.preventDefault()
+      SetCursorY(Array.from(linesContainer.childNodes).findIndex((l) => l === line))
+      SetCursorX(event.offsetX / 10.81)
+    })
+  }
   return line;
 }
 
 if (linesContainer.childNodes.length < 1) {
-  const firstLine = newLine();
+  const firstLine = newLine(true);
   linesContainer.appendChild(firstLine)
 }
 
-function interpretSpecial(key) {
-  if (key.startsWith("Arrow")) {
+function interpretSpecial(event) {
+  if (event.key.startsWith("Arrow")) {
     const focus = document.activeElement;
     if (focus.classList.contains("line")) {
-      switch (key.replace("Arrow", "")) {
+      switch (event.key.replace("Arrow", "")) {
         case "Up":
           {
             if (focus.previousSibling) {
@@ -101,30 +139,53 @@ function interpretSpecial(key) {
       }
     }
   }
-  if (key == "Backspace") {
+  if (event.key == "Backspace") {
     const focus = document.activeElement;
     if (focus.classList.contains("line")) {
-      focus.textContent = focus.textContent.splice(cursor.x - 1, 1)
-      moveCursorLeft();
+      if (cursor.x > 0) {
+        focus.textContent = focus.textContent.splice(cursor.x - 1, 1)
+        moveCursorLeft();
+      } else if (cursor.y > 0) {
+        deleteLine(cursor.y)
+        moveCursorUp()
+        focusLine(cursor.y)
+        SetCursorX(Infinity)
+      }
       return true;
     }
   }
-  if (key == "Delete") {
+  if (event.key == "Delete") {
     const focus = document.activeElement;
     if (focus.classList.contains("line")) {
-      focus.textContent = focus.textContent.splice(cursor.x, 1)
-      return true;
+      if (cursor.x == focus.textContent.length) {
+        if (lineExists(cursor.y + 1)) {
+          focus.textContent = focus.textContent + focus.nextSibling.textContent
+          deleteLine(cursor.y + 1)
+        }
+        return true
+      } else {
+        focus.textContent = focus.textContent.splice(cursor.x, 1)
+        return true;
+      }
     }
   }
-  if (key == "Enter") {
+  if (event.key == "Enter") {
     const focus = document.activeElement;
     if (focus.classList.contains("line")) {
-      const line = newLine();
+      //TODO enter when middle of line
+      const line = newLine(true);
       focus.parentElement.insertBefore(line, focus.nextSibling)
       focus.nextSibling.focus();
       moveCursorDown();
       return true;
     }
+  }
+  if (event.key == "Tab") {
+    event.preventDefault();
+    return "   ";
+  }
+  if (event.key == "Shift") {
+    return true;
   }
   return false;
 }
